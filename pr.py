716c4a8efff3c647e7487d0b9866a0bcd59d1076
PePaPeBPa <140555013+PePaPeBPa@users.noreply.github.com>
2023-08-04 21:01:00 +0200

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,10 @@
+a = input("Insrisci il primo numero ")
+b = input ("Inserisci il secondo numero ")
+if a > b:
+    print("Il numero più grande é il primo inserito")
+elif b > a:
+    print("Il numero più grande é il secondo inserito")
+else:
+    print("I numeri sono identici")
+
+    
