9bc01ea15e4af64ea8f67608dc0fe4284775b755
PePaPeBPa <140555013+PePaPeBPa@users.noreply.github.com>
2023-08-04 21:00:05 +0200

    Delete pr.py

--- pr.py
+++
@@ -1,9 +0,0 @@
-a = input("Insrisci il primo numero")
-b = input ("Inserisci il secondo numero")
-if a > b:
-    print(a)
-elif b > a:
-    print(b)
-else:
-    print("I numeri sono identici")
-    
