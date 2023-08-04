d3ac4e295fbf6ae74998bab263c33bcc26220700
PePaPeBPa <140555013+PePaPeBPa@users.noreply.github.com>
2023-08-04 17:59:36 +0200

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,9 @@
+a = input("Insrisci il primo numero")
+b = input ("Inserisci il secondo numero")
+if a > b:
+    print(a)
+elif b > a:
+    print(b)
+else:
+    print("I numeri sono identici")
+    
